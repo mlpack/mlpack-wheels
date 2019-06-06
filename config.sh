94e33370333b0be7026e6fef5290190c2f3b6e78
Ryan Curtin <ryan@ratml.org>
2019-06-06 19:34:39 -0400

    Initial configuration test.

---
+++ config.sh
@@ -0,0 +1,14 @@
+# Custom functionality for multibuilds.
+
+function pre_build
+{
+  # I have no idea what we'll need here yet...
+  pwd
+  ls
+}
+
+function run_test
+{
+  # I also have no idea what we should do here yet...
+  python --version
+}
