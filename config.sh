0b0fc6785d3661e693aad8cf7032840889ce70e9
Ryan Curtin <ryan@ratml.org>
2019-06-06 20:41:19 -0400

    CMake is too old on RHEL5, but I'm not convinced 2.8 will work...

--- config.sh
+++ config.sh
@@ -4,7 +4,7 @@ function pre_build
 {
   # We need to get mlpack dependencies.  We are root inside the container, and
   # this is RHEL5.
-  yum install -y wget cmake make gcc-c++ boost-devel openblas
+  yum install -y wget cmake28 make gcc-c++ boost-devel openblas
 
   # Now download Armadillo and install that from a known non-HTTPS source
   # (because we don't have working HTTPS).
