ccd59877cb8278dd9ef31a04975f8510d66757db
Ryan Curtin <ryan@ratml.org>
2019-06-06 21:32:19 -0400

    Build CMake from source.

--- config.sh
+++ config.sh
@@ -4,14 +4,23 @@ function pre_build
 {
   # We need to get mlpack dependencies.  We are root inside the container, and
   # this is RHEL5.
-  yum install -y wget cmake28 make gcc-c++ boost-devel openblas
+  yum install -y wget make gcc-c++ boost-devel openblas
+
+  # Ha, because RHEL5 is so old we have to build CMake from source.  But we have
+  # to use an older CMake version because new CMake versions don't build right.
+  wget http://www.ratml.org/misc/cmake-3.13.5.tar.gz
+  tar -xvzpf cmake-3.13.5.tar.gz
+  cd cmake-3.13.5/
+  ./configure
+  gmake -j4
+  make install
 
   # Now download Armadillo and install that from a known non-HTTPS source
   # (because we don't have working HTTPS).
   wget http://www.ratml.org/misc/armadillo-9.400.4.tar.gz
-  tar -xvpf armadillo-9.400.4.tar.gz
+  tar -xvzpf armadillo-9.400.4.tar.gz
   cd armadillo-9.400.4
-  cmake28 .
+  cmake .
   make
   cd ../
 
@@ -19,7 +28,7 @@ function pre_build
   cd mlpack/
   mkdir build
   cd build/
-  cmake28 \
+  cmake \
       -DARMADILLO_INCLUDE_DIR=../../armadillo-9.400.4/tmp/include/ \
       -DARMADILLO_LIBRARY=../../armadillo-9.400.4/libarmadillo.so \
       ../
