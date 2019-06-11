1eea3071bb8ee6992b52f08f2afe3ac3f67ec8e1
Ryan Curtin <ryan@ratml.org>
2019-06-11 00:35:23 -0400

    Use precompiled RPMs.

--- config.sh
+++ config.sh
@@ -9,36 +9,13 @@ function pre_build
   # this is RHEL5.
   yum install -y wget make gcc-c++ openblas
 
-  # We need to get our Python dependencies using the desired pip version.
-  # (hardcoded for now)
-  /opt/python/cp27-cp27m/bin/pip install Cython numpy pandas
-
-  # Ha, because RHEL5 is so old we have to build CMake from source.  But we have
-  # to use an older CMake version because new CMake versions don't build right.
-  wget http://www.ratml.org/misc/cmake-3.13.5.tar.gz
-  tar -xzpf cmake-3.13.5.tar.gz
-  cd cmake-3.13.5/
-  ./configure --prefix=/usr
-  gmake -j4
-  make install
-  cd ../
-
-  # Now download Armadillo and install that from a known non-HTTPS source
-  # (because we don't have working HTTPS).
-  wget http://www.ratml.org/misc/armadillo-9.400.4.tar.gz
-  tar -xzpf armadillo-9.400.4.tar.gz
-  cd armadillo-9.400.4
-  cmake -DCMAKE_INSTALL_PREFIX=/usr .
-  make install
-  cd ../
-
-  # We also need to download boost.
-  wget http://www.ratml.org/misc/boost_1_70_0.tar.gz
-  tar -xzpf boost_1_70_0.tar.gz
-  cd boost_1_70_0/
-  ./bootstrap.sh --with-libraries=serialization,program_options,test
-  ./b2 -j4 install --prefix=/usr
-  cd ../
+  # Install RPMs that were manually made for this image.
+  wget http://www.ratml.org/misc/cmake-3.13.5-1.x86_64.rpm
+  rpm -ivh cmake-3.13.5-1.x86_64.rpm
+  wget http://www.ratml.org/misc/armadillo-9.400.4-1.x86_64.rpm
+  rpm -ivh armadillo-9.400.4-1.x86_64.rpm
+  wget http://www.ratml.org/misc/boost-1.70.0-1.x86_64.rpm
+  rpm -ivh boost-1.70.0-1.x86_64.rpm
 
   # Finally let's go ahead and build mlpack.
   cd mlpack/
