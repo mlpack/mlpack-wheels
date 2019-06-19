b83a28feac95d2cfb01b6cb2efd8ce083a858a54
Ryan Curtin <ryan@ratml.org>
2019-06-19 14:04:31 -0400

    Further debugging.

--- config.sh
+++ config.sh
@@ -76,6 +76,8 @@ function build_libs {
     # Sudo needed for macOS
     local use_sudo=""
     [ -n "$IS_OSX" ] && use_sudo="sudo"
+    echo "plat $plat tar_path $tar_path use_sudo $use_sudo";
+    ls -l $tar_path
     (cd / && $use_sudo tar zxf $tar_path)
 }
 
