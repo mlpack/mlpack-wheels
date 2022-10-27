fc9d51c0b2c0dcbef9c68ab04f7a55af48ae0c3d
Ryan Curtin <ryan@ratml.org>
2022-10-27 18:17:47 -0400

    Install necessary dependencies.

--- build_mlpack.sh
+++ build_mlpack.sh
@@ -2,6 +2,8 @@
 #
 # Build mlpack's Python bindings inside the cibuildwheel environment.
 
+sudo apt-get install libopenblas-dev libarmadillo-dev libcereal-dev libensmallen-dev
+
 cd mlpack/build/
 rm -rf *
 cmake -DBUILD_PYTHON_BINDINGS=ON ../
