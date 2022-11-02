1ad3155a547cbd5ce006e7fa7c2a8c390563440d
Ryan Curtin <ryan@ratml.org>
2022-11-02 11:55:26 -0400

    Install dependencies by hand on Alpine.

--- build_mlpack.musl.sh
+++ build_mlpack.musl.sh
@@ -2,8 +2,27 @@
 #
 # Build mlpack's Python bindings inside the cibuildwheel musllinux environment.
 
-apk add openblas-dev armadillo-dev cereal
+# Enable the community repository.
+apk add openblas-dev
 pip install cython numpy pandas
+
+# Armadillo must be built by hand.
+wget https://files.mlpack.org/armadillo-11.4.1.tar.gz
+tar -xvzpf armadillo-11.4.1.tar.gz
+cd armadillo*/
+cmake -DCMAKE_INSTALL_PREFIX=/usr ../
+make
+make install
+cd ../
+rm -rf armadillo*/
+
+# cereal must be installed by hand.
+wget https://github.com/USCILab/cereal/archive/refs/tags/v1.3.2.tar.gz
+tar -xvzpf v1.3.2.tar.gz
+cd cereal*/
+cp -vr include/* /usr/include/
+cd ../
+rm -rf cereal* v1.3.2.tar.gz
 
 # ensmallen must be built by hand.
 wget https://www.ensmallen.org/files/ensmallen-latest.tar.gz
