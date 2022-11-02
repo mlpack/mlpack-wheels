11a4754ce6ab1c889a9bc2725321c49acc09e560
Ryan Curtin <ryan@ratml.org>
2022-11-02 09:08:08 -0400

    Attempt to refactor to add musllinux, osx, and Windows builds.

---
+++ build_mlpack.musl.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#
+# Build mlpack's Python bindings inside the cibuildwheel musllinux environment.
+
+apk install openblas-dev armadillo-dev cereal
+pip install cython numpy pandas
+
+# ensmallen must be built by hand.
+wget https://www.ensmallen.org/files/ensmallen-latest.tar.gz
+tar -xvzpf ensmallen-latest.tar.gz
+cd ensmallen*/
+cmake -DBUILD_TESTS=OFF -DCMAKE_INSTALL_PREFIX=/usr ../
+make install
+cd ../
+rm -rf ensmallen*
+
+cd mlpack/
+patch -p1 < ../reduce-lib-size.patch
+cd build/
+rm -rf *
+cmake -DBUILD_PYTHON_BINDINGS=ON ../
+make -j4
