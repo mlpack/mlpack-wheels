901a667d1a7ce3929612dbadd0b83b7a947d72f5
Ryan Curtin <ryan@ratml.org>
2022-11-02 13:51:52 -0400

    Try avoiding wildcards.

--- build_mlpack.musl.sh
+++ build_mlpack.musl.sh
@@ -9,29 +9,29 @@ pip install cython numpy pandas
 # Armadillo must be built by hand.
 wget https://files.mlpack.org/armadillo-11.4.1.tar.gz
 tar -xvzpf armadillo-11.4.1.tar.gz
-cd armadillo*/
-cmake -DCMAKE_INSTALL_PREFIX=/usr ../
+cd armadillo-11.4.1/
+cmake -DCMAKE_INSTALL_PREFIX=/usr .
 make
 make install
 cd ../
-rm -rf armadillo*/
+rm -rf armadillo-11.4.1/ armadillo-11.4.1.tar.gz
 
 # cereal must be installed by hand.
 wget https://github.com/USCILab/cereal/archive/refs/tags/v1.3.2.tar.gz
 tar -xvzpf v1.3.2.tar.gz
-cd cereal*/
+cd cereal-1.3.2/
 cp -vr include/* /usr/include/
 cd ../
-rm -rf cereal* v1.3.2.tar.gz
+rm -rf cereal-1.3.2 v1.3.2.tar.gz
 
 # ensmallen must be built by hand.
-wget https://www.ensmallen.org/files/ensmallen-latest.tar.gz
-tar -xvzpf ensmallen-latest.tar.gz
-cd ensmallen*/
+wget https://www.ensmallen.org/files/ensmallen-2.19.0.tar.gz
+tar -xvzpf ensmallen-2.19.0.tar.gz
+cd ensmallen-2.19.0/
 cmake -DBUILD_TESTS=OFF -DCMAKE_INSTALL_PREFIX=/usr ../
 make install
 cd ../
-rm -rf ensmallen*
+rm -rf ensmallen-2.19.0 ensmallen-2.19.0.tar.gz
 
 cd mlpack/
 patch -p1 < ../reduce-lib-size.patch
