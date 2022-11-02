206a3d659a7f34aa8831e68ce92aa20c75f7a882
Ryan Curtin <ryan@ratml.org>
2022-11-02 10:05:58 -0400

    Build armadillo and ensmallen by hand for i686.

--- build_mlpack.i686.sh
+++ build_mlpack.i686.sh
@@ -4,8 +4,27 @@
 # package.
 
 # RHEL7 on i686 does not have openblas-devel; so, we use atlas-devel instead.
-yum install -y atlas-devel armadillo-devel cereal-devel ensmallen-devel
+# Armadillo and ensmallen must both be installed by hand.
+yum install -y atlas-devel cereal-devel
 pip install cython numpy pandas
+
+wget https://files.mlpack.org/armadillo-11.4.1.tar.gz
+tar -xvzpf armadillo-11.4.1.tar.gz
+cd armadillo*/
+cmake -DCMAKE_INSTALL_PREFIX=/usr ../
+make
+make install
+cd ../
+rm -rf armadillo*/
+
+wget https://www.ensmallen.org/files/ensmallen-2.19.0.tar.gz
+tar -xvzpf ensmallen-2.19.0.tar.gz
+cd ensmallen*/
+cmake -DCMAKE_INSTALL_PREFIX=/usr -DBUILD_TESTS=OFF ../
+make
+make install
+cd ../
+rm -rf ensmallen*
 
 cd mlpack/
 patch -p1 < ../reduce-lib-size.patch
