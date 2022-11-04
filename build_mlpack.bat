a79112d88a01ea2ba72a39c0063b54134904d851
Ryan Curtin <ryan@ratml.org>
2022-11-04 09:24:05 -0400

    Attempt to fix Windows build paths.

--- build_mlpack.bat
+++ build_mlpack.bat
@@ -6,12 +6,12 @@ rm -rf build/
 mkdir build
 cd build/
 cmake -G "Visual Studio 16 2019" ^
-    -DBLAS_LIBRARIES:FILEPATH=$(Agent.ToolsDirectory)\OpenBLAS.0.2.14.1\lib\native\lib\x64\libopenblas.dll.a ^
-    -DLAPACK_LIBRARIES:FILEPATH=$(Agent.ToolsDirectory)\OpenBLAS.0.2.14.1\lib\native\lib\x64\libopenblas.dll.a ^
+    -DBLAS_LIBRARIES:FILEPATH="..\..\OpenBLAS.0.2.14.1\lib\native\lib\x64\libopenblas.dll.a" ^
+    -DLAPACK_LIBRARIES:FILEPATH="..\..\OpenBLAS.0.2.14.1\lib\native\lib\x64\libopenblas.dll.a" ^
     -DARMADILLO_INCLUDE_DIR="..\..\armadillo-11.4.1\tmp\include" ^
     -DARMADILLO_LIBRARY="..\..\armadillo-11.4.1\Release\armadillo.lib" ^
-    -DCEREAL_INCLUDE_DIR=$(Agent.ToolsDirectory)\unofficial-flayan-cereal.1.2.2\build\native\include ^
-    -DENSMALLEN_INCLUDE_DIR=$(Agent.ToolsDirectory)\ensmallen.2.17.0\installed\x64-linux\include ^
+    -DCEREAL_INCLUDE_DIR="..\..\cereal.1.3.2\include" ^
+    -DENSMALLEN_INCLUDE_DIR="..\..\ensmallen.2.19.0\include" ^
     -DBUILD_CLI_EXECUTABLES=OFF ^
     -DBUILD_JULIA_BINDINGS=OFF ^
     -DBUILD_PYTHON_BINDINGS=ON ^
