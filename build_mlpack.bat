86d16e6f1aa220def761772e087631ad127cfe63
Ryan Curtin <ryan@ratml.org>
2022-11-06 21:11:52 -0500

    %cd% does not appear to have the right value at that point.

--- build_mlpack.bat
+++ build_mlpack.bat
@@ -1,5 +1,9 @@
 pip install cython numpy pandas
 
+echo "cd at start"
+echo "%cd%"
+set rootdir=%cd%
+
 cd mlpack/
 patch -p1 < ../reduce-lib-size.patch
 patch -p1 < ../static-binding-linking.patch
@@ -10,16 +14,13 @@ cd build/
 mkdir Release
 cp ..\..\OpenBLAS-0.3.21\bin\libopenblas.dll Release/
 
-echo "cd at start"
-echo "%cd%"
-
 cmake -G "Visual Studio 16 2019" ^
-    -DBLAS_LIBRARIES:FILEPATH="%cd%\OpenBLAS-0.3.21\lib\libopenblas.lib" ^
-    -DLAPACK_LIBRARIES:FILEPATH="%cd%\OpenBLAS-0.3.21\lib\libopenblas.lib" ^
-    -DARMADILLO_INCLUDE_DIR="%cd%\armadillo-11.4.1\tmp\include" ^
-    -DARMADILLO_LIBRARY="%cd%\armadillo-11.4.1\Release\armadillo.lib" ^
-    -DCEREAL_INCLUDE_DIR="%cd%\cereal-1.3.2\include" ^
-    -DENSMALLEN_INCLUDE_DIR="%cd%\ensmallen-2.19.0\include" ^
+    -DBLAS_LIBRARIES:FILEPATH="%rootdir%\OpenBLAS-0.3.21\lib\libopenblas.lib" ^
+    -DLAPACK_LIBRARIES:FILEPATH="%rootdir%\OpenBLAS-0.3.21\lib\libopenblas.lib" ^
+    -DARMADILLO_INCLUDE_DIR="%rootdir%\armadillo-11.4.1\tmp\include" ^
+    -DARMADILLO_LIBRARY="%rootdir%\armadillo-11.4.1\Release\armadillo.lib" ^
+    -DCEREAL_INCLUDE_DIR="%rootdir%\cereal-1.3.2\include" ^
+    -DENSMALLEN_INCLUDE_DIR="%rootdir%\ensmallen-2.19.0\include" ^
     -DBUILD_CLI_EXECUTABLES=OFF ^
     -DBUILD_JULIA_BINDINGS=OFF ^
     -DBUILD_PYTHON_BINDINGS=ON ^
