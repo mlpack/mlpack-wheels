d75df1a3b0649b2a909001166dc7f4c90c635908
Ryan Curtin <ryan@ratml.org>
2022-11-08 09:27:44 -0500

    Add some comments to the Windows build script.

--- build_mlpack.bat
+++ build_mlpack.bat
@@ -1,7 +1,11 @@
+rem Build mlpack's Python bindings on a Windows system.
+
 pip install cython numpy pandas delvewheel
 
+rem This is needed later for copying libopenblas.dll to the right place.
 set rootdir=%cd%
 
+rem Patch the code, clean any old build directory.
 cd mlpack/
 patch -p1 < ../reduce-lib-size.patch
 patch -p1 < ../static-binding-linking.patch
@@ -9,6 +13,7 @@ rmdir /s /q build
 mkdir build
 cd build/
 
+rem Configure CMake and build the Python bindings.
 cmake -G "Visual Studio 16 2019" ^
     -DBLAS_LIBRARIES:FILEPATH="%rootdir%\OpenBLAS-0.3.21\lib\libopenblas.lib" ^
     -DLAPACK_LIBRARIES:FILEPATH="%rootdir%\OpenBLAS-0.3.21\lib\libopenblas.lib" ^
@@ -21,8 +26,12 @@ cmake -G "Visual Studio 16 2019" ^
     -DBUILD_PYTHON_BINDINGS=ON ^
     -DBUILD_SHARED_LIBS=OFF ^
     -DCMAKE_BUILD_TYPE=Release ..
-cmake --build . --target python --config Release -- -verbosity:detailed
+cmake --build . --target python --config Release
 
+rem The tests cannot be run correctly by cibuildwheel because libopenblas.dll
+rem must be in the right directory; so, we copy libopenblas.dll to the right
+rem place and manually run a simple test here to ensure that everything is
+rem working.
 cd src\mlpack\bindings\python
 cp %rootdir%\OpenBLAS-0.3.21\bin\libopenblas.dll .
 set PYTHONPATH=%PYTHONPATH%;%rootdir%\mlpack\build\src\mlpack\bindings\python
