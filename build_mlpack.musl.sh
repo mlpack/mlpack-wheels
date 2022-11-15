fe42da510e095261d51d4c53a8ffc0540eeb346d
Ryan Curtin <ryan@ratml.org>
2022-11-15 09:05:56 -0500

    Specify include directory for musl builds too.

--- build_mlpack.musl.sh
+++ build_mlpack.musl.sh
@@ -46,5 +46,5 @@ cd mlpack/
 patch -p1 < ../reduce-lib-size.patch
 cd build/
 rm -rf *
-cmake -DBUILD_PYTHON_BINDINGS=ON -DBUILD_CLI_EXECUTABLES=OFF ../
+cmake -DBUILD_PYTHON_BINDINGS=ON -DBUILD_CLI_EXECUTABLES=OFF -DSTB_IMAGE_INCLUDE_DIR=/usr/include/ ../
 make -j$NUM_CORES
