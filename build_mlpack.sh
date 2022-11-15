890588a84c8c9da123cda9f0571f9217db8edfb1
Ryan Curtin <ryan@ratml.org>
2022-11-15 09:06:36 -0500

    Always build on one core to try and avoid OOM killings.

--- build_mlpack.sh
+++ build_mlpack.sh
@@ -16,4 +16,4 @@ patch -p1 < ../reduce-lib-size.patch
 cd build/
 rm -rf *
 cmake -DBUILD_PYTHON_BINDINGS=ON -DBUILD_CLI_EXECUTABLES=OFF -DSTB_IMAGE_INCLUDE_DIR=/usr/include/ ../
-make -j$NUM_CORES
+make
