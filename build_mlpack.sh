b73e7ade585828140a62374426288132264afa32
Ryan Curtin <ryan@ratml.org>
2024-06-06 14:59:08 -0400

    Install setuptools manually.

--- build_mlpack.sh
+++ build_mlpack.sh
@@ -5,7 +5,7 @@
 set -e -u -o pipefail
 
 yum install -y openblas-devel armadillo-devel cereal-devel ensmallen-devel wget
-pip install cython numpy pandas wheel
+pip install cython numpy pandas wheel setuptools
 
 # STB must be installed by hand.
 wget https://www.mlpack.org/files/stb.tar.gz
